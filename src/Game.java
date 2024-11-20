028e5931e68142606c391a0608ee77c8201e0f8a
clairel0103 <clairel25@nycstudents.net>
2024-11-20 13:19:29 -0500

    11/20

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,40 @@
 import java.util.Random;
+import java.util.*;
 
 public class Game{
+
+    private int x;
+    private int y;
+    private List <String> operations = Arrays.asList("+", "-", "*", "/", "^");
+
     public Game(int level_num){
         int count = 0;
+        int round = 0;
         }
-    public void answer(int x, int y, String operation){
 
+    public int answer(int x, int y, String operation){
+        int answer = 0;
+        if(operation.equals("+")){
+            answer = x+y;
+        }
+        else if (operation.equals("-")) {
+            answer = x-y;
+        }
+        else if (operation.equals('*')){
+            answer = x*y;
+        }
+        else if (operation.equals('/')){
+            answer = x*y;
+        }
+        else if (operation.equals('^')){
+            double x_dbl = (double)x;
+            double y_dbl = (double)y;
+            answer = (int)Math.pow(x_dbl, y_dbl);
+        }
+        return answer;
+    }
+
+    public problem_generate(){
+        x = rand.int;
     }
 }
